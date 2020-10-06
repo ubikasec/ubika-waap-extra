b79cb2802cf61937cdfe864475562a8575d60706
Sylvain Firmery <sylvain.firmery@rohde-schwarz.com>
2020-10-06 11:36:13 +0200

    feat: terraform files for GCP

---
+++ terraform/modules/gcp/_/managed/main.tf
@@ -0,0 +1,94 @@
+variable "context" {}
+variable "management_private_ip" {}
+variable "additional_tags" { default = [] }
+
+resource "google_compute_firewall" "managed_from_management" {
+  name        = "managed-from-management"
+  description = "Enable RS WAF Administration access from the Management instance"
+  network     = var.context.vpc
+
+  direction   = "INGRESS"
+  source_tags = ["rswaf-management"]
+  target_tags = ["rswaf-managed"]
+
+  allow {
+    protocol = "tcp"
+    ports    = ["2222", "3001", "48400", "22"]
+  }
+}
+resource "google_compute_firewall" "managed_admin" {
+  name        = "managed-admin"
+  description = "Enable RS WAF Administration access from the admin_location"
+  network     = var.context.vpc
+
+  direction     = "INGRESS"
+  source_ranges = [var.context.admin_location]
+  target_tags   = ["rswaf-managed"]
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+}
+
+
+# Managed instances
+resource "google_compute_region_instance_group_manager" "managed" {
+  name = "${var.context.name_prefix}-managed"
+
+  base_instance_name        = "${var.context.name_prefix}-managed"
+  region                    = var.context.region
+  distribution_policy_zones = var.context.zones
+
+  target_pools = var.context.target_pools
+  target_size  = var.context.nb_managed
+
+  version {
+    name              = "main"
+    instance_template = "${google_compute_instance_template.managed.self_link}"
+  }
+
+
+  update_policy {
+    type                         = "OPPORTUNISTIC"
+    instance_redistribution_type = "NONE"
+    minimal_action               = "RESTART"
+    max_unavailable_fixed        = length(var.context.zones)
+  }
+}
+
+resource "google_compute_instance_template" "managed" {
+  name_prefix = "${var.context.name_prefix}-managed-template"
+
+  instance_description = "${var.context.name_prefix} managed"
+  machine_type         = var.context.managed_instance_type
+
+  tags = concat(["rswaf-managed"], var.additional_tags)
+
+  disk {
+    boot         = true
+    source_image = var.context.images.managed
+    disk_size_gb = var.context.disk_size.managed
+  }
+
+  network_interface {
+    network = var.context.vpc
+    access_config {}
+  }
+
+  metadata = {
+    user-data = jsonencode({
+      instance_role = "managed"
+      instance_name = "managed-##"
+      linkto_ip     = "${var.management_private_ip}"
+      linkto_port   = "3001"
+      linkto_apikey = "${var.context.autoreg_admin_apikey}"
+    })
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+output "template" { value = google_compute_instance_template.managed }
---
+++ terraform/modules/gcp/basic/common_variables.tf
@@ -0,0 +1,48 @@
+# GCP common
+
+variable "vpc" {}
+variable "region" { default = "" }
+variable "zones" { default = [] }
+variable "target_pools" {}
+
+# RSWAF Settings
+
+variable "product_version" { default = "" }
+
+variable "name_prefix" {
+  default = "rswaf-cloud"
+}
+
+variable "cluster_name" { default = "" }
+
+variable "admin_location" {
+  description = "The IP address range that can be used to administrate the WAF instances"
+  default     = "0.0.0.0/0"
+}
+
+variable "admin_user" { default = "admin" }
+variable "admin_pwd" { default = "" }
+variable "admin_apiuid" { default = "" }
+
+variable "autoreg_admin_user" { default = "autoreg_admin" }
+variable "autoreg_admin_apiuid" { default = "" }
+
+# Management instance
+variable "management_mode" {
+  description = "Enter management instance license mode \"Bring Your Own License\" or \"PAYG\". Default is \"byol\"."
+  default     = "byol"
+}
+variable "management_instance_type" { default = "n1-standard-4" }
+variable "additional_management_tags" { default = [] }
+variable "management_disk_size" { default = 120 }
+
+# Managed instances
+variable "managed_mode" {
+  description = "Enter managed instance license mode \"Bring Your Own License\" or \"PAYG\". Default is \"byol\"."
+  default     = "byol"
+}
+variable "managed_instance_type" { default = "n1-standard-2" }
+variable "additional_managed_tags" { default = [] }
+variable "managed_disk_size" { default = 30 }
+
+variable "nb_managed" { default = "0" }
