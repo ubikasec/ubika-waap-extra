78458b30ea6b10dd9c23fa127972ac5139b0f718
Matthieu Vaidie <matthieu.vaidie@ubikasec.com>
2026-08-14 15:04:05 +0200

    feat: update outscale tf file for 6.16

--- terraform/latest/examples/outscale_basic/main.tf
+++ terraform/latest/examples/outscale_basic/main.tf
@@ -143,7 +143,7 @@ module "ubikawaap" {
 
   autoreg_admin_apiuid = "6a9f6424ca12dfd25ad4ac82a459e332" # an API key (32 random alphanum chars)
 
-  product_version = "6.11.8" # product version to select instance images, changing it will recreate all instances
+  product_version = "6.16.3" # product version to select instance images, changing it will recreate all instances
 
   management_mode          = "byol"      # WAAP licence type of the management instance ("payg" or "byol")
   management_instance_type = "tinav4.c4r16p2" # management AWS instance type
@@ -151,7 +151,7 @@ module "ubikawaap" {
 
   managed_mode          = "byol"      # WAAP licence type of the managed instances ("payg" or "byol")
   managed_instance_type = "tinav4.c2r4p2" # managed AWS instance type
-  managed_disk_size     = 30          # size of the managed disk in GiB (default to 30GiB)
+  managed_disk_size     = 60          # size of the managed disk in GiB (default to 60GiB)
 
   nb_managed = 1 # number of managed instances
 }
